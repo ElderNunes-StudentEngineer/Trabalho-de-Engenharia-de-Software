8d3cc86a7f5c790d39412afe977bd114084ea1d0
Guilherme Giuliangeli Monteiro <guigiumon@gmail.com>
2026-04-10 16:54:51 -0300

    Metodos para mudança de status e tipo

--- vaga.py
+++ vaga.py
@@ -6,3 +6,10 @@ class Vaga:
     tipo: str
     status: str
 
+    def alterar_status(self, novo_status: str):
+            self.status = novo_status
+            print(f"Vaga {self.id}: Status atualizado para {self.status}")
+
+    def alterar_tipo(self, novo_tipo: str):
+        self.tipo = novo_tipo
+        print(f"Vaga {self.id}: Tipo atualizado para {self.tipo}")
